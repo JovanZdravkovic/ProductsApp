6d2ab44e797d892603681ba6d815d8523663c49d
JovanZdravkovic <jovan.zdravkovic882@gmail.com>
2024-12-16 21:28:56 +0100

    Added alert icons in alert banners.

--- app_flutter_frontend/lib/alertbanner.dart
+++ app_flutter_frontend/lib/alertbanner.dart
@@ -6,29 +6,41 @@ export 'alertbanner.dart';
 
 class AlertBanner {
   static MaterialBanner alertBanner(BuildContext context, String bannerMessage, int statusCode) {
-    dynamic typeColor;
+    var contextPrimaryColor = Theme.of(context).colorScheme.onPrimary;
+    dynamic typeColor, iconType;
     // statusCode == 0 ==> Success banner (green)
     // statusCode == 1 ==> Error banner (red)
     // statusCode != 0,1 ==> Info banner (blue)
     switch (statusCode) {
       case 0:
         typeColor = Colors.green.shade300;
+        iconType = Icon(Icons.check_circle_outline, color: contextPrimaryColor);
         break; 
       case 1: 
         typeColor = Colors.red.shade600;
+        iconType = Icon(Icons.error_outline, color: contextPrimaryColor);
         break; 
       default:
         typeColor = Colors.blue;
+        iconType = Icon(Icons.info_outline, color: contextPrimaryColor);
     }
     return MaterialBanner(
-      content: Text(bannerMessage, style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),),
+      content: Row(
+        children: [
+          iconType,
+          const SizedBox(
+            width: 10.0,
+          ),
+          Text(bannerMessage, style: TextStyle(color: contextPrimaryColor),)
+        ],
+      ),
       backgroundColor: typeColor,
       actions: <Widget>[
         TextButton(
           onPressed: () {
             ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
           }, 
-          child: Icon(Icons.close, color: Theme.of(context).colorScheme.onPrimary),
+          child: Icon(Icons.close, color: contextPrimaryColor),
         ),
       ],
     );
